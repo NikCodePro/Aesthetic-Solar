263b71635affc4ce8735380189ac7fba2feee248
Nikhil Verma <113196745+NikCodePro@users.noreply.github.com>
2024-03-05 22:13:22 +0530

    Add files via upload

---
+++ Aesthetic Solar/static/js/script.js
@@ -0,0 +1,39 @@
+// navbar section -----------------------------------
+
+var bar = document.getElementById("bar");
+var close = document.getElementById("close");
+var nav = document.getElementById("navbar");
+if (bar) {
+    bar.addEventListener("click", () => {
+        nav.classList.add("active");
+    })
+}
+if (close) {
+    close.addEventListener("click", () => {
+        nav.classList.remove("active");
+    })
+}
+
+// swiper js carousal
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
